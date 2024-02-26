6ae40b45171c318b5728b64dd82c56cd3af1b3c0
William Applegate <w.applegate@ymail.com>
2024-02-25 22:57:31 -0500

    added some styling to login and register

--- public/js/library.js
+++ public/js/library.js
@@ -1,3 +1,20 @@
+
+jQuery(document).ready(function() {
+    jQuery('#registerForm').submit(function(event) {
+        event.preventDefault();
+
+        var password = jQuery('#password').val();
+        var confirmPassword = jQuery('#passwordConfirmation').val();
+
+        if(password !== confirmPassword) {
+            jQuery('#error').show();
+        } else {
+            jQuery('#error').hide();
+            jQuery(this).unbind('submit').submit();
+        }
+    })
+});
+
 const logout = () => {
     fetch('auth/logout', {
         method: 'POST',
