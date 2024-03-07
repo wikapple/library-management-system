f45182764ad730da6da58bca9d21f8988f30ee46
William Applegate <w.applegate@ymail.com>
2024-03-06 21:46:15 -0500

    Sending a new database script

--- scripts/InitializeLibraryDb_AutoGenScript.sql
+++ scripts/InitializeLibraryDb_AutoGenScript.sql
@@ -28,19 +28,19 @@ CREATE TABLE IF NOT EXISTS `baserentalitem` (
   `name` varchar(255) NOT NULL,
   `itemType` enum('MEDIA','INSTRUMENT') NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table librarydb.baserentalitem: ~6 rows (approximately)
+-- Dumping data for table librarydb.baserentalitem: ~8 rows (approximately)
 DELETE FROM `baserentalitem`;
 INSERT INTO `baserentalitem` (`id`, `description`, `name`, `itemType`) VALUES
 	(7, 'Dracula is a novel by Bram Stoker, published in 1897. An epistolary novel, the narrative is related through letters, diary entries, and newspaper articles. It has no single protagonist and opens with solicitor Jonathan Harker taking a business trip to sta', 'Dracula', 'MEDIA'),
-	(8, '        The novel tells the story of Dracula\'s attempt to move from Transylvania to England so that he may find new blood and spread the undead curse, and of the battle between Dracula and a small group of men and a woman led by Professor Abraham Van Helsing.    ', 'Dracula', 'MEDIA'),
+	(8, 'The novel tells the story of Dracula\'s attempt to move from Transylvania to England so that he may find new blood and spread the undead curse, and of the battle between Dracula and a small group of men and a woman led by Professor Abraham Van Helsing.      ', 'Dracula', 'MEDIA'),
 	(9, 'A dazzling new edition of J.K. Rowling\'s Harry Potter and the Sorcerer\'s Stone, fully illustrated in brilliant color and featuring exclusive interactive paper craft elements, including a foldout Hogwarts letter and more! In this stunning new edition of Harry Potter and the Sorcerer\'s Stone, experience the story as never before. J.K. Rowling\'s complete and unabridged text is accompanied by full-color illustrations on nearly every page and eight exclusive, interactive paper craft elements: Readers will open Harry\'s Hogwarts letter, reveal the magical entryway to Diagon Alley, make a sumptuous feast appear in the Great Hall, and more.Designed and illustrated by award-winning design studio MinaLima -- best known for establishing the visual graphic style of the Harry Potter and Fantastic Beasts films -- this edition is sure to be a keepsake for Harry Potter fans, a beautiful addition to any collector\'s bookshelf, and an enchanting way to introduce the first book in this beloved series to a new generation of readers.', 'Harry Potter and The Sorcerer\'s Stone', 'MEDIA'),
 	(10, 'War and Peace centers broadly on Napoleon’s invasion of Russia in 1812 and follows three of the best-known characters in literature: Pierre Bezukhov, the illegitimate son of a count who is fighting for his inheritance and yearning for spiritual fulfillment; Prince Andrei Bolkonsky, who leaves behind his family to fight in the war against Napoleon; and Natasha Rostov, the beautiful young daughter of a nobleman, who intrigues both men. As Napoleon’s army invades, Tolstoy vividly follows characters from diverse backgrounds—peasants and nobility, civilians and soldiers—as they struggle with the problems unique to their era, their history, and their culture. And as the novel progresses, these characters transcend their specificity, becoming some of the most moving—and human—figures in world literature. Pevear and Volokhonsky have brought us this classic novel in a translation remarkable for its fidelity to Tolstoy’s style and cadence and for its energetic, accessible prose.', 'War and Peace', 'MEDIA'),
 	(11, 'George and Harold are fourth-grade buddies with a penchant for practical jokes. When the boys\' latest prank drives their science teacher over the edge, their clueless principal, Mr. Krupp, quickly hires a replacement: Professor Pippy P. Poopypants. Of course, George and Harold can\'t resist making fun of the Professor\'s silly name. But then the Professor retaliates by forcing everyone in town to change their own names to be equally silly, with colossal consequences!', 'Captain Underpants And The Perilous Plot Of Professor Poopypants', 'MEDIA'),
 	(12, 'Hailed by Dostoyevsky as "The final and greatest utterance of the human mind," Don Quixote constitutes a founding work of modern Western literature. Cervantes\' masterpiece has been translated into more than sixty languages, and the novel\'s fantasy-driven "knight," Don Quixote, and his loyal squire, Sancho Panza, rank among fiction\'s most recognized characters. Their adventures have been interpreted for film, opera, and ballet, and they head a cast of characters that comprises a diverse array of beliefs and perspectives, reflecting the historical realities of seventeenth-century Spain. Within its absorbing re-creation of the world during Cervantes\' time, this parody of chivalric romances and epic of heroic idealism forms a strikingly contemporary narrative. The author is often credited with inventing the concept of the novel, addressing himself to the readers rather than the characters or the action. This edition of his landmark work presents the acclaimed 1755 Tobias Smollett translation.', 'Don Quixote', 'MEDIA'),
-	(22, ' efgegf', 'eafgea', 'MEDIA'),
-	(24, '     A thrilling history of the Queen City of the West during the Civil War.\r\nThe Man Who Saved Cincinnati...\r\nRescued the Queen City from a Confederate attack;\r\nFormed the first Black Brigade in the Union Army;\r\nSaved Washington, DC;\r\nCaptured Billy the Kid and stopped a bloody range war;\r\nWrote one of the greatest books in American literature, Ben-Hur.\r\nAlso: The secret plot to assassinate Abraham Lincoln in Cincinnati;\r\nPanic and threats from Morgan\'s Raiders;\r\nHow the scapegoat of Shiloh became the Savior of Cincinnati.  ', 'The Man Who Saved Cincinnati', 'MEDIA');
+	(24, '     A thrilling history of the Queen City of the West during the Civil War.\r\nThe Man Who Saved Cincinnati...\r\nRescued the Queen City from a Confederate attack;\r\nFormed the first Black Brigade in the Union Army;\r\nSaved Washington, DC;\r\nCaptured Billy the Kid and stopped a bloody range war;\r\nWrote one of the greatest books in American literature, Ben-Hur.\r\nAlso: The secret plot to assassinate Abraham Lincoln in Cincinnati;\r\nPanic and threats from Morgan\'s Raiders;\r\nHow the scapegoat of Shiloh became the Savior of Cincinnati.  ', 'The Man Who Saved Cincinnati', 'MEDIA'),
+	(39, 'The Hobbit is set in Middle-earth and follows home-loving Bilbo Baggins, the hobbit of the title, who joins the wizard Gandalf and the thirteen dwarves of Thorin\'s Company, on a quest to reclaim the dwarves\' home and treasure from the dragon Smaug.', 'The Hobbit', 'MEDIA');
 
 -- Dumping structure for table librarydb.category
 DROP TABLE IF EXISTS `category`;
@@ -130,9 +130,9 @@ CREATE TABLE IF NOT EXISTS `libraryuser` (
   PRIMARY KEY (`UserId`),
   KEY `UserRoleId` (`UserRoleId`),
   CONSTRAINT `libraryuser_ibfk_1` FOREIGN KEY (`UserRoleId`) REFERENCES `userrole` (`RoleId`) ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table librarydb.libraryuser: ~10 rows (approximately)
+-- Dumping data for table librarydb.libraryuser: ~11 rows (approximately)
 DELETE FROM `libraryuser`;
 INSERT INTO `libraryuser` (`UserId`, `Name`, `PhoneNumber`, `Email`, `PasswordHash`, `DateOfBirth`, `UserRoleId`) VALUES
 	(1, 'admin', '111-222-3333', 'admin@admin.com', '$2a$12$PbFqKsS1gGmPuMHPus6jIOst0RpmOTkji8s2tAyxUyGglh7tMu3Ny', '1990-01-01', 3),
@@ -140,11 +140,11 @@ INSERT INTO `libraryuser` (`UserId`, `Name`, `PhoneNumber`, `Email`, `PasswordHa
 	(3, 'member', '987-654-3210', 'member@member.com', '$2a$12$AQ6qrVf0HPFdL.9hsLVkAOrnu4kr/2CIuE5i..zdmGkUbR7SghR5e', '2000-10-10', 1),
 	(4, 'Kris', '111-222-3333', 'kris@kris.com', '$2b$10$GOqfDR3Ke3GJIA2hErzLruBdyAIr/DVVULtJHDh7vy56VKmfnYCKi', '1990-05-05', 1),
 	(5, 'William Applegate', '15026485380', 'w.applegate@ymail.com', '$2b$10$w5XjP2fmf9iYUl8.6fUToOwVB8/.icMDT3/O6JEode94.PqNtDleq', '1190-05-05', 1),
-	(6, 'William Applegate', '5026485380', 'wikapple@iu.edu', '$2b$10$yA790ucqKk8lEu68xIyrBuXPOFBblJeQnoD1KtWTp8Ei7oFLk6V4a', '2024-02-25', 1),
 	(7, 'William Applegate', '5026485380', 'apples@apples.com', '$2b$10$ymiZs0bC3WtjIK/rB5HEsuUIQJD3pXA63/PUvdRnKVKrmU.UVj9c2', '2024-02-25', 1),
 	(8, 'John', '5026485380', 'wikapple@gmail.com', '$2b$10$NRqBU4q76b2lnoNOZV0VYe41XmggTTKd/EZ/vX6slKwCgcr6P7LEO', '2024-02-26', 1),
 	(9, 'Kyle', '1234132451232', 'wefa@eafa.com', '$2b$10$L8dQN.dOrWLv5Yvvag3ef.nnrCpft8cVEqEAdfGS6ynkdEVJ4ecgi', '2024-02-26', 1),
-	(10, 'Matt', '123-456-1234', 'matt@matthew.com', '$2b$10$olKlcya6kiYEbjAD.bd1E.zlcDolOddK.rqIHdZwoOr87KjlY3mT6', '2024-02-26', 1);
+	(10, 'Matt', '123-456-1234', 'matt@matthew.com', '$2b$10$olKlcya6kiYEbjAD.bd1E.zlcDolOddK.rqIHdZwoOr87KjlY3mT6', '2024-02-26', 1),
+	(12, 'William Applegate', '502-648-5380', 'wikapple@iu.edu', '$2b$10$h2G81ek0A.ydC9WPYT2mNOjhEOyr4JnVxjq8csq6xk1hQyP3GBG4a', '1990-05-05', 1);
 
 -- Dumping structure for table librarydb.media
 DROP TABLE IF EXISTS `media`;
@@ -162,7 +162,7 @@ CREATE TABLE IF NOT EXISTS `media` (
   CONSTRAINT `media_mediaType_FK` FOREIGN KEY (`typeId`) REFERENCES `mediatype` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table librarydb.media: ~5 rows (approximately)
+-- Dumping data for table librarydb.media: ~7 rows (approximately)
 DELETE FROM `media`;
 INSERT INTO `media` (`uniqueIdentifier`, `publisher`, `isChildSafe`, `baseRentalItemId`, `typeId`, `pageCountOrSize`, `author`) VALUES
 	('9780194789431', 'Random House', b'0', 8, 1, 417, 'Bram Stoker'),
@@ -170,8 +170,8 @@ INSERT INTO `media` (`uniqueIdentifier`, `publisher`, `isChildSafe`, `baseRental
 	('978-1400079988', 'Vintage', b'1', 10, 1, 1296, 'Leo Tolstoy'),
 	('978-1338864328', 'Scholastic Inc.', b'1', 11, 1, 160, 'Dav Pilkey'),
 	('978-0486821955', 'Dover Publications', b'1', 12, 1, 992, 'Miguel de Cervantes'),
-	('r353f', 'efgae', b'1', 22, 4, 214, 'eeg'),
-	('979-8988581710', 'Chilidog Press LLC', b'1', 24, 1, 294, 'Peter Bronson');
+	('979-8988581710', 'Chilidog Press LLC', b'1', 24, 1, 294, 'Peter Bronson'),
+	('978-026110334', 'Harper Collins', b'1', 39, 1, 288, 'J.R.R. Tolkien');
 
 -- Dumping structure for table librarydb.mediacategories
 DROP TABLE IF EXISTS `mediacategories`;
@@ -184,17 +184,18 @@ CREATE TABLE IF NOT EXISTS `mediacategories` (
   CONSTRAINT `mediaFK` FOREIGN KEY (`mediaId`) REFERENCES `media` (`baseRentalItemId`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table librarydb.mediacategories: ~20 rows (approximately)
+-- Dumping data for table librarydb.mediacategories: ~27 rows (approximately)
 DELETE FROM `mediacategories`;
 INSERT INTO `mediacategories` (`mediaId`, `categoryId`) VALUES
 	(8, 28),
 	(9, 29),
 	(12, 29),
-	(22, 30),
+	(39, 29),
 	(10, 32),
 	(10, 33),
 	(8, 34),
 	(9, 37),
+	(39, 37),
 	(24, 38),
 	(11, 44),
 	(12, 44),
